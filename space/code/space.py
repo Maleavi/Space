3f39cbf8245d552024790008fa99681edd989d34
Maleavi <manueltohl@gmail.com>
2019-11-25 10:18:43 +0100

    files uploaded
    very cool

---
+++ space/code/space.py
@@ -0,0 +1,55 @@
+import math
+import numpy as num
+from openpyxl import Workbook
+
+def gravity(posistion):
+    grav = -posistion
+    sum = 0
+    for i in grav:
+        sum = sum + (i * i)
+
+    sum = math.sqrt(sum)
+    return (grav / (sum * sum * sum))
+
+def array_reader(array_thing):
+    for o in array_thing:
+        yield o
+    
+#xlsx writer
+wb = Workbook()
+dest_filename = 'resources/data.xlsx'
+ws1 = wb.active
+ws2 = wb.create_sheet()
+ws3 = wb.create_sheet()
+
+ws1.title = 'orbital positions'
+ws2.title = 'orbital velocities'
+ws3.title = 'gravity'
+
+#parameters
+pos = num.array([-1.0,0.0])
+vel = num.array([0.0, 1.5])
+actuness = 512
+steps = 1024
+
+grav = 0
+
+for i in range(steps):
+    pos = pos + (vel / actuness)
+    vel = vel + (gravity(pos) / (actuness))
+    grav = 0
+
+    for d in gravity(pos):
+        grav = grav + (d * d)
+
+    grav = math.sqrt(grav)
+
+    print (pos)
+    print (vel)
+    print (grav)
+    print ('')
+    ws1.append(array_reader(pos))
+    ws2.append(array_reader(vel))
+    ws3.append([grav])
+
+wb.save(filename = dest_filename)
