01aec8ad8fe9975b07ff7bd8d61184c97327b444
Maleavi <manueltohl@gmail.com>
2019-12-09 01:13:00 +0100

    new super version
    everythings more great!
    super great space objects now!

--- space/code/space.py
+++ space/code/space.py
@@ -1,12 +1,35 @@
 import math
 import numpy as np
 
-def gravity(posistion, mass = 1):
-    grav = -posistion * mass
-    sum = 0
-    for i in grav:
-        sum = sum + (i * i)
+class space_object(object):
+    def __init__(self, mass, position = np.array([0.0, 0.0]), velocity = np.array([0.0, 0.0])):
+        self.mass = mass
+        self.pos = position
+        self.vel = velocity
 
-    sum = math.sqrt(sum)
-    return (grav / (sum * sum * sum))
-    
+    def grav(self, pos):
+        rtn = (self.pos-pos) 
+
+        return (rtn * self.mass) / (math.sqrt(rtn[0]**2 + rtn[1]**2)**3)
+
+    def move(self, act):
+        self.pos = self.pos + self.vel * act
+
+    def accelerate(self, force, act):
+        self.vel = self.vel + (force * act / self.mass)
+
+
+#def gravity(posistion, mass = 1):
+#    grav = -posistion * mass
+#    sum = 0
+#    for i in grav:
+#        sum = sum + (i * i)
+#
+#    sum = math.sqrt(sum)
+#    return (grav / (sum * sum * sum))
+#
+#def array_reader(array_thing):
+#    #numpy doing shenenigans, so just made a generator for that
+#    for o in array_thing:
+#        yield o
+#
