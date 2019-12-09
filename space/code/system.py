feb8113c86734cc8e342be6d3c71ae8cde8542d3
Maleavi <manueltohl@gmail.com>
2019-12-09 08:32:52 +0100

    better system.py
    updated body speedes; dont mess with 3 bodies, they'll just kick themselfs out of existance

--- space/code/system.py
+++ space/code/system.py
@@ -15,20 +15,25 @@ ws1 = wb.active
 ws2 = wb.create_sheet()
 ws3 = wb.create_sheet()
 ws4 = wb.create_sheet()
+#ws5 = wb.create_sheet()
+#ws6 = wb.create_sheet()
 
 ws1.title = 'orbital position obj1'
 ws2.title = 'orbital velocitie obj1'
 ws3.title = 'orbital position obj2'
 ws4.title = 'orbital velocitie obj2'
+#ws5.title = 'orbital position obj3'
+#ws6.title = 'orbital velocitie obj3'
 
 #parameters
 pos = np.array([-1.0,0.0])
-vel = np.array([0.0, 1.0])
+vel = np.array([0.0, 0.5])
 actuness = 512
-steps = 1024
+steps = 8192
 
 space_objects = []
-space_objects.append(space.space_object(1))
+space_objects.append(space.space_object(1, np.array([0.0, 0.0]), np.array([0.0, -0.5])))
+#space_objects.append(space.space_object(1, np.array([0.0, -1.0]), np.array([0.0, -0.5])))
 space_objects.append(space.space_object(1, pos, vel))
 
 
@@ -50,9 +55,14 @@ for i in range(steps):
     print (space_objects[1].pos)
     print (space_objects[1].vel)
     print('')
+    #print (space_objects[2].pos)
+    #print (space_objects[2].vel)
+    print('')
     ws1.append(array_reader(space_objects[0].pos))
     ws2.append(array_reader(space_objects[0].vel))
     ws3.append(array_reader(space_objects[1].pos))
     ws4.append(array_reader(space_objects[1].vel))
+    #ws5.append(array_reader(space_objects[2].pos))
+    #ws6.append(array_reader(space_objects[2].vel))
 
 wb.save(filename = dest_filename)
